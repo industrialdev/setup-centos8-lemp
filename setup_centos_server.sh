4df0e9bd6e76fbe7b10972c8ca02dba7afc9d55d
rferguson <rferguson@industrialagency.ca>
2020-06-29 11:11:23 -0400

    bumped software versions and corrected for centos8

--- setup_centos_server.sh
+++ setup_centos_server.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+sudo yum -y update
 sudo yum install -y epel-release
-sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-7.rpm
+sudo rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-8.rpm
 sudo cp mariadb.repo /etc/yum.repos.d
-sudo rpm -Uvh http://nginx.org/packages/centos/7/x86_64/RPMS/nginx-1.16.1-1.el7.ngx.x86_64.rpm
-sudo yum install -y zip unzip git composer java-1.8.0-openjdk mariadb-server php php-fpm php-mysqlnd php-gd php-curl php-mbstring php-dom php-opcache php-soap --enablerepo=remi-php73
+sudo rpm -Uvh http://nginx.org/packages/centos/8/x86_64/RPMS/nginx-1.18.0-1.el8.ngx.x86_64.rpm
+sudo yum -y module enable php:remi-7.4
+sudo yum install -y zip unzip git composer java-1.8.0-openjdk mariadb-server php php-fpm php-mysqlnd php-gd php-curl php-mbstring php-dom php-opcache php-soap --enablerepo=remi
 sudo systemctl enable nginx && sudo systemctl start nginx && sudo systemctl enable mariadb && sudo systemctl start mariadb && sudo systemctl enable php-fpm && sudo systemctl start php-fpm
 mysqladmin -u root password root
 sudo sed -i "$ a [mariadb] \nmax_allowed_packet=900M" /etc/my.cnf
@@ -18,12 +20,12 @@ sudo cp nginx.conf /etc/nginx/conf.d
 composer global require drush/drush
 echo 'export PATH=$HOME/.config/composer/vendor/bin:$PATH' >> ~/.bash_profile
 source ~/.bash_profile
-sudo rpm -Uvh https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.3.2-x86_64.rpm
+sudo rpm -Uvh https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.4.0-x86_64.rpm
 yes | sudo /usr/share/elasticsearch/bin/elasticsearch-plugin install ingest-attachment
 sudo systemctl enable elasticsearch && sudo systemctl start elasticsearch
 sudo semanage permissive -a httpd_t
 sudo firewall-cmd --permanent --add-port=80/tcp
-sudo firewall-cmd --permanent --add-port=443/tcp   
+sudo firewall-cmd --permanent --add-port=443/tcp
 sudo firewall-cmd --reload
 sudo cp /usr/share/zoneinfo/America/New_York /etc/localtime
 sudo systemctl restart nginx && sudo systemctl restart mariadb && sudo systemctl restart php-fpm
@@ -39,9 +41,3 @@ DROP DATABASE IF EXISTS test;
 DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';
 FLUSH PRIVILEGES;
 EOF
-
-
-
-
-
-
